8a6e4ae25dcfc4e368a0c87bcb3b01f1dd5bfe94
kingame96 <truongthinh619@gmail.com>
2020-02-06 15:28:11 +0700

    second change

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,6 @@
-import React from 'react.development' ;
-import ReactDOM from 'react-dom.development' ;
-
-class Test extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return(
-            <div>
-                this is some text
-            </div>
-        )
-    }
+$(document).ready(function() {
+    $("p").click(function(){
+        $(this).hide();
+    });
 }
-
-ReactDOM.render(<Test />, document.getElementById('container'));
+);
